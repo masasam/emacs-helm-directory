c0bf39c18307253c52ccb88f535fe39c537353d8
Masashi Miyaura <masasam@users.noreply.github.com>
2017-06-20 16:11:01 +0900

    Trial implementation

--- helm-phalcon.el
+++ helm-phalcon.el
@@ -26,9 +26,66 @@
 
 ;;; Code:
 
+(require 'helm)
+(require 'helm-mode)
+(require 'helm-files)
+
 (defgroup helm-phalcon nil
   "phalcon complete with helm interface."
   :group 'helm)
+
+(defcustom helm-phalcon-basedir nil
+  "Directory where Phalcon Framework is placed."
+  :group 'helm-phalcon
+  :type 'string)
+
+(defun helm-phalcon--open-dired (file)
+  "Open file with dired as FILE."
+  (dired (file-name-directory file)))
+
+(defvar helm-phalcon--action
+  '(("Open File" . find-file)
+    ("Open Directory" . helm-phalcon--open-dired)))
+
+(defun helm-phalcon--list-candidates ()
+  "Helm list candidates."
+  (with-temp-buffer
+    (let ((paths))
+      (push (concat helm-phalcon-basedir "app/models/entities") paths)
+      (push (concat helm-phalcon-basedir "app/models/repositories/Repository") paths)
+      (push (concat helm-phalcon-basedir "app/models/services/Service") paths)
+      (push (concat helm-phalcon-basedir "app/messages") paths)
+      (push (concat helm-phalcon-basedir "app/config") paths)
+      (push (concat helm-phalcon-basedir "app/modules/frontend/controllers") paths)
+      (push (concat helm-phalcon-basedir "app/modules/frontend/controllers/Admin") paths)
+      (push (concat helm-phalcon-basedir "app/utils/Forms") paths)
+      (push (list-directory (concat helm-phalcon-basedir "app/modules/frontend/views")) paths)
+      (reverse paths))))
+
+(defun helm-phalcon--source (repo)
+  "Helm source as REPO."
+  (let ((name (file-name-nondirectory (directory-file-name repo))))
+    (helm-build-in-buffer-source name
+      :init #'helm-phalcon--ls-files
+      :action helm-phalcon--action)))
+
+(defun helm-phalcon--ls-files ()
+  "Helm ls."
+  (with-current-buffer (helm-candidate-buffer 'global)
+    (unless (zerop (apply #'call-process ls nil '(t nil) nil))
+      (error "Failed: Can't get file list candidates"))))
+
+;;;###autoload
+(defun helm-phalcon ()
+  "Phalcon complete with helm interface."
+  (interactive)
+  (let ((repo (helm-comp-read "phalcon-list: "
+                              (helm-phalcon--list-candidates)
+                              :name "phalcon list"
+                              :must-match t)))
+    (let ((default-directory (file-name-as-directory repo)))
+      (helm :sources (list (helm-phalcon--source default-directory))
+            :buffer "*helm-phalcon-list*"))))
 
 (provide 'helm-phalcon)
 
